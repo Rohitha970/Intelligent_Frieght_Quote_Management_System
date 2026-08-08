e610ae6928828a56b126259b1fe8c672f2f27734
T Rohitha <thogururohitha123@gmail.com>
2026-08-08 21:32:14 +0530

    Update urls.py

--- backend/freight_project/urls.py
+++ backend/freight_project/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home_view, name='home'),
+    
     
     # Matching React endpoint base paths
     path('api/', include('authentication.urls')),
