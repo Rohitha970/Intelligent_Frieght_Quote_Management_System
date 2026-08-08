cceb77ec3730b3eeaa29bdbb3af958e6062cd435
T Rohitha <thogururohitha123@gmail.com>
2026-08-08 19:35:33 +0530

    Fix python path in wsgi.py

--- backend/freight_project/wsgi.py
+++ backend/freight_project/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for freight_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+from pathlib import Path
+
+# Add the backend folder to sys.path
+BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(str(BASE_DIR))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freight_project.settings')
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "freight_project.settings")
-
 application = get_wsgi_application()
