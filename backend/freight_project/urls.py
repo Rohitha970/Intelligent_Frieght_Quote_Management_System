ad3968c3b909cbd63e51aa76a510386dab5b9987
T Rohitha <thogururohitha123@gmail.com>
2026-08-08 21:44:48 +0530

    Update urls.py

--- backend/freight_project/urls.py
+++ backend/freight_project/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-
+def home(request):
+    return HttpResponse("API Server is Live and Running! 🚀")
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    path('', home, name='home'),
     
     # Matching React endpoint base paths
     path('api/', include('authentication.urls')),
